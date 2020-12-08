f91064d33bf54b53c59d33cdd0b12edabb4cdecc
antran1245 <antran1245@gmail.com>
2020-12-08 14:40:55 -0800

    initial setup

---
+++ src/database/Classes.js
@@ -0,0 +1,26 @@
+export const Classes = [
+    {
+        id:1,
+        title: "Computer Literacy",
+        division: "Lower",
+        CourseNumber: "2"
+    },
+    {
+        id:2,
+        title: "Introduction to Programming in C/C++",
+        division: "Lower",
+        CourseNumber: "5C"
+    },
+    {
+        id:3,
+        title: "Introduction to Programming in Java. F,W,S",
+        division: "Lower",
+        CourseNumber: "5J"
+    },
+    {
+        id:4,
+        title: "Algorithms and Abstract Data Types. F,W,S",
+        division: "Upper",
+        CourseNumber: "101"
+    }
+]
